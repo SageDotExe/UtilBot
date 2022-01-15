1f9f5bdf75caa5682556a23c030f71407d600051
SageDotExe <sagerainbow27@gmail.com>
2022-01-16 00:31:27 +1300

    e

--- commands/fun/badgerSpam.js
+++ commands/fun/badgerSpam.js
@@ -4,11 +4,13 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
+        wait(300)
         message.channel.send("<@763150093494386698>")
         message.channel.send("<@763150093494386698>")
         message.channel.send("<@763150093494386698>")
         message.channel.send("<@763150093494386698>")
         message.channel.send("<@763150093494386698>")
+        wait(5)
         message.channel.send('sorry for making this but BTW EVERYONE IF U WANT TO TROLL BADGER AND MAKE HIM MAD DO THE FOLLOWING COMMAND : s.spamb again sorry badger <3')
     }
 }

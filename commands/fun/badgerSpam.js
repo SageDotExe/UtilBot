be7bb48f671e96261806e6457d583e6d185f693f
SageDotExe <sagerainbow27@gmail.com>
2022-01-16 00:24:11 +1300

    Added A Command to spam friend thats it i think..

---
+++ commands/fun/badgerSpam.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "spamb",
+    category: "fun",
+    permissions: [],
+    devOnly: false,
+    run: async ({client, message, args}) => {
+        message.channel.send("<@763150093494386698>")
+        message.channel.send("<@763150093494386698>")
+        message.channel.send("<@763150093494386698>")
+        message.channel.send("<@763150093494386698>")
+        message.channel.send("<@763150093494386698>")
+    }
+}
